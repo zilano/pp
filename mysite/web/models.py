4d204e0e892bc888034fa3f8ff2dea7e2af69c85
zilano <neonozila@gmail.com>
2018-08-10 06:25:53 +0430

    income model added

--- mysite/web/models.py
+++ mysite/web/models.py
@@ -6,4 +6,9 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete = models.DO_NOTHING)
-     
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete = models.DO_NOTHING)

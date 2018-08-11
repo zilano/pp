4dcc35784001f3bbcbb39f51f603dcfa00e62010
zilano <neonozila@gmail.com>
2018-08-11 04:57:53 +0430

    models become better

--- mysite/web/models.py
+++ mysite/web/models.py
@@ -6,9 +6,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete = models.DO_NOTHING)
+    def __str__(self):
+        return "{} - {}".format(self.date, self.amount)
 
 class Income(models.Model):
     text = models.CharField(max_length = 255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete = models.DO_NOTHING)
+    def __str__(self):
+        return "{} - {}".format(self.date, self.amount)
